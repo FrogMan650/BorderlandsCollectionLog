78a297d3ab2336e3d0009d1a9f1e98d29db93846
LucasK650 <123999867+FrogMan650@users.noreply.github.com>
2026-09-07 10:56:15 -0400

    Deb
    Created a Deb installer.

--- src/main/java/com/frogman650/App.java
+++ src/main/java/com/frogman650/App.java
@@ -279,6 +279,8 @@ public class App extends Application {
                 itemsDirectory = new File(executableDirectory.getParent(), "lib/items");
             } else if (executableDirectory.toString().toLowerCase().contains("/app/bin")) {
                 itemsDirectory = new File(executableDirectory.getParent(), "share/BorderlandsCollectionLog/items");
+            } else if (executableDirectory.toString().toLowerCase().contains("/usr/bin")) {
+                itemsDirectory = new File(executableDirectory.getParent(), "share/BorderlandsCollectionLog/items");
             } else {
                 itemsDirectory = new File(executableDirectory, "items");
             }
